9c5627b6fbec0c2d320a2f8b9ac115d2c38d66be
lmoonwolf <kutaykurt99@gmail.com>
2021-09-19 23:23:41 +0300

    code optimizations

--- index.js
+++ index.js
@@ -12,19 +12,21 @@ const chalk = require("chalk"); //Chalk - console color lib
 let look = new Lookup();
 
 look.on("detected", (light) => {
-  if (light.id == "0x0000000012a52889") {
-    runSync(light);
-  }
+  //Run sync when light detected (Check light.id here if you have more than one lights)
+  runSync(light);
 });
 
 async function runSync(light) {
   let color;
   let img;
 
+  // Loop every second
   setInterval(loop, 1000);
 
   async function loop() {
+    // Get screenshot
     img = await screenshot();
+    // Get average color of the image
     average(img, (err, res) => {
       if (err) throw err;
       color = res;
@@ -32,6 +34,7 @@ async function runSync(light) {
         chalk.bold.rgb(color[0], color[1], color[2])(`Color found: ${color}`)
       );
     });
+    // Set light to that color
     light
       .setRGB(color, 1000)
       .then(() => {
